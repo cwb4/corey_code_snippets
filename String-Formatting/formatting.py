c6705c876d051134099cee2e0b4743e8d150e511
cwb4 <cwb4@cwb4s-air.home>
2020-06-05 10:34:27 +0100

    misc

--- String-Formatting/formatting.py
+++ String-Formatting/formatting.py
@@ -72,6 +72,6 @@ print(sentence)
 
 # March 01, 2016 fell on a Tuesday and was the 061 day of the year.
 
-sentence = '{:%B %d, %Y} fell on a {} and was the {} day of the year'.format(my_date)
+# sentence = '{:%B} fell on a {%d} and was the {%Y} day of the year'.format(my_date)
 
 print(sentence)
